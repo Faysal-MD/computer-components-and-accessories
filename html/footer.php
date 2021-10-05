92b58b80d16761a56f1b5fe969d70e7db94b0078
Faysal075 <faysalmahmud735@gmail.com>
2021-10-05 10:35:46 +0600

    htmlToPhp

---
+++ html/footer.php
@@ -0,0 +1,23 @@
+<section class="container">
+    <div class=" d-flex justify-content-around" style="margin-left: 3rem;">
+        <div class="">
+            <i class="text-center fas fa-map-marker-alt fa-2x mt-3"></i>
+            <p class="getInTouchText text-white mt-3 text-center" style="">123 Street, New York, USA</p>
+        </div>
+        <div class="">
+            <i class="fas fa-headset fa-2x mt-3"></i>
+            <p class="getInTouchText text-white mt-3" style="margin-left: 10px;">+880 1521260328</p>
+        </div>
+        <div class="">
+            <i class="fas fa-envelope fa-2x mt-3"></i>
+            <p class="getInTouchText text-white mt-3" style="margin-left: 5px;">180204061@aust.edu</p>
+        </div>
+    </div>
+</section>
+    <p class="text-center" style="color: white">© Copyright 2021, FT Solutions, BD</p>
+    <div class=" d-flex justify-content-center">
+        <img class="mb-3" src="../images/facebook.png" alt="">
+        <img class="mb-3" src="../images/instagram.png" alt="">
+        <img class="mb-3" src="../images/twitter.png" alt="">
+    </div>
+    <!-- <i class="far fa-arrow-alt-circle-up fa-4x" style="margin-left: 1100px"></i> -->
